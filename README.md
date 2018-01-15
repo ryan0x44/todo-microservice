c8ccae39403b7864d2b479facc868ddb355ca4ce
Ryan D <git@ryan0x44.com>
2018-01-15 01:05:05 +0000

    Add mysql and docker/compose files

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# todo-microservice-experiments
-Experiments of how to build a Todo Microservice
+# Todo Microservice experiments
+
+This repository contains experiments of building a Todo Microservice.
+
+Each implementation of Todo Microservice should support a multi-transport,
+multi-version API using HTTP JSON and gRPC/Protobufs.
+
+The goal is that by doing so, we can demonstrate the strength and weaknesses of
+various tools/frameworks/libraries/patterns.
+
+## Domain Model
+
+We keep it simple:
+
+* Projects have a name
+* Tasks have a description
+* Projects can have many tasks assigned
+
+# Docker
+
+We have a `docker-compose.yml` file which enables us to quickly build and
+run each service with only Docker and Docker Compose as dependencies.
+
+e.g. `docker-compose up -d --build` (run without `--build` to cache builds)
+
+To correctly shutdown containers and clean-up the database volume:
+
+e.g. `docker-compose down --volumes`
+
+Our set-up includes MySQL with the DB initialized from `database/todo.sql`.
+
+## HTTP Contract Tests
+
+We use [newman](https://github.com/postmanlabs/newman) to run contract tests.
+
+e.g. `docker run -t postman/newman_ubuntu1404 run contract-tests`
+
+## Implementations
+
+__Go Kit__
+
+Uses <http://gokit.io/>
---
+++ database/todo.sql
@@ -0,0 +1,51 @@
+# ************************************************************
+# Database: todo
+# Generation Time: 2018-01-15 00:44:04 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table projects
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `projects`;
+
+CREATE TABLE `projects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table tasks
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tasks`;
+
+CREATE TABLE `tasks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `description` text NOT NULL,
+  `project_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `task_project` (`project_id`),
+  CONSTRAINT `task_project` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
